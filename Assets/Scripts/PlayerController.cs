4fcb12d4c9a86dda323cc21357a8e2a27fa2e8a2
Joakim Nilsson <joani718@student.liu.se>
2017-05-06 00:08:41 +0200

    player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool hidden;
+
+    public Color color;
+    public float speed = 6.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+
+
+        if (Input.GetButtonDown("A"))
+        {
+            
+        }else if (Input.GetButtonDown("A"))
+        {
+
+        }
+    }
+
+}
