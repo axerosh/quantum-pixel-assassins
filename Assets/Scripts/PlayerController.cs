9104640dfa9679c863cee569a3cc2b7fca334fcb
Joakim Nilsson <joani718@student.liu.se>
2017-05-06 10:57:31 +0200

    controller fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public bool hidden;
+    public int playerNumber;
+
+    MeshRenderer ren;
 
     public Material Red;
     public Material Green;
@@ -12,32 +15,32 @@ public class PlayerController : MonoBehaviour {
     public float speed = 6.0F;
     private Vector3 moveDirection = Vector3.zero;
 
-    MeshRenderer ren;
+    void Update()
+    {
+        Rigidbody2D controller = GetComponent<Rigidbody2D>();
+
+        float moveHorizontal = Input.GetAxis("Horizontal" + playerNumber.ToString());
+        float moveVertical = Input.GetAxis("Vertical" + playerNumber.ToString());
+        
+
+        Vector3 movment = new Vector3(moveHorizontal, moveVertical, 0);
+        controller.velocity = movment * speed;
+
+        if (Input.GetButtonDown("A"))
+        {
+            ren.sharedMaterial = Green;
+        }
+        else if (Input.GetButtonDown("B"))
+        {
+            ren.sharedMaterial = Red;
+        }
+
+    }
 
     void Start()
     {
         ren = GetComponent<MeshRenderer>();
         ren.enabled = true;
-    }
-
-    void Update()
-    {
-        Rigidbody2D controller = GetComponent<Rigidbody2D>();
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movment = new Vector3(moveHorizontal, moveVertical, 0);
-        controller.velocity = movment * speed;
-
-
-        if (Input.GetButtonDown("A"))
-        {
-            ren.sharedMaterial = Green;
-        }else if (Input.GetButtonDown("B"))
-        {
-            ren.sharedMaterial = Red;
-        }
     }
 
 }
