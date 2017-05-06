2ce360ea7d8cd32db75c6a9c78d7cd111b848924
Joakim Nilsson <joani718@student.liu.se>
2017-05-06 14:34:29 +0200

    added interactible

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public ToggleInteractible toggleInteractible;
+    public int a;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (toggleInteractible.GetToggled())
+        {
+            Toggle();
+        }
+	}
+
+    public void Toggle()
+    {
+        transform.Rotate(0, 0, 90);
+    }
+}
